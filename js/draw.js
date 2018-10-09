8b4993dee94974f46697a8f44b4d4035fc15c083
xindi-wang <wangcd94@gmail.com>
2018-10-08 21:42:19 -0400

    check

--- js/draw.js
+++ js/draw.js
@@ -3,45 +3,73 @@ $(document).ready(function () {
     loadData();
 
 });
+var sort
 
 // Loads the CSV file
 function loadData() {
    d3.csv("data/candy-data.csv",function(d){
      data = d;
      updateData();
-     console.log(data);
-     visualizeChart(data);
+     Order(sort);
+
    })
 }
 
+
 function updateData() {
-
-  // var item = data;
-
+  var newdata = [];
   $('input[type=radio]').each(function(){
     $(this).change(function(){
       //data = data;
       if (this.checked) {
         id = $(this)[0].id;
-        data.filter(function (d) {
+        item = data.filter(function (d) {
           return d[id] == 1;
       });
-      console.log(data);
+      //console.log(item);
+      newdata = item;
+      console.log(newdata);
+      $("#chart").empty();
+      visualizeChart(newdata);
     };
-
-
    })
   })
- //console.log(ndata);
+  //visualizeChart(newdata);
+  //console.log(newdata);
+
+  // d3.selectAll(".button").on("click", function () {
+  //     sort= d3.select(this).attr("data-val");
+  //   })
+
+
+//console.log(document.getElementsByClassName("current").innerHTML);
+
+
+
+
+function Order(sort){
+
+  switch (order) {
+    case "by-rank":
+      data.sort((a, b) => a.winpercent - b.winpercent); break;
+    case "by-price":
+      data.sort((a, b) => a.pricepercent - b.pricepercent); break;
+    case "by-sugar":
+      data.sort((a, b) => a.sugarpercent - b.sugarpercent); break;
+  }
+  x.domain(data.map(d => d.name));
+  chart.update();
+  return order;
 }
+
 
 function visualizeChart(item) {
 
   //console.log(item);
 
-  var margin = { top: 0, right: 0, bottom: 20, left: 0 };
-  var width = 600;
-  var height = 300;
+  var margin = { top: 0, right: 100, bottom: 20, left: 100 };
+  var width = 1000 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
 
   var x = d3.scaleBand()
     .domain(item.map(function(d) {
@@ -58,9 +86,32 @@ function visualizeChart(item) {
 
   var svg = d3.select("#chart")
      .append("svg")
-     .attr("width", "90%")
-     .attr("height", 1000)
+     .attr("width", 800)
+     .attr("height", 600)
      //.attr("id", "container")
+
+   svg.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x))
+     .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", function (d) {
+    return "rotate(-45)";
+});
+
+   // add the y Axis
+   yaxis = svg.append("g")
+     .call(d3.axisLeft(y))
+
+  xaxis = svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x));
+
+   //yaxis.ticks(10);
+
+
   var rects = svg.selectAll(".bar")
    .data(item)
    .enter()
@@ -78,6 +129,21 @@ function visualizeChart(item) {
      return height - y(d.winpercent);
    });
 
-
-
+//    svg.node().update = () => {
+//     const t = svg.transition()
+//         .duration(750);
+//
+//     bar.data(data, d => d.name)
+//         .order()
+//       .transition(t)
+//         .delay((d, i) => i * 20)
+//         .attr("x", d => x(d.name));
+//
+//     xaxis.transition(t)
+//         .call(xAxis)
+//       .selectAll(".tick")
+//         .delay((d, i) => i * 20);
+//
+//
+// }
 }
